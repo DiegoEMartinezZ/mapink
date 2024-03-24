0e87e5282f4c131c46c474ecfb90aa8a8a8deb15
DiegoEMartinezZ <diermaza.23@gmail.com>
2024-03-24 18:33:54 -0500

    adding hover to the logo home

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,7 +6,11 @@ const NavBar = () => {
     <nav>
       <ul className="scroll-smooth bg-black-950 z-20 text-black-500 fixed top-0 right-0 left-0 flex flex-nowrap items-center  justify-around ">
         <a href="/">
-          <img src="/favicon-32x32.png" alt="Home" className="opacity-50 h-7" />
+          <img
+            src="/favicon-32x32.png"
+            alt="Home"
+            className="opacity-50 h-7 hover:opacity-100"
+          />
         </a>
         <BtnNavBar name={"Home"} section={"QuienesSomos"} />
         <BtnNavBar name={"Catálogo"} section={"Catalogo"} />
